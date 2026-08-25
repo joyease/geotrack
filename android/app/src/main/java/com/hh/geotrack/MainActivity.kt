b0c09782998018d7cb89ffed72711dd8ea2e8711
Joyease <hermannhuang@gmail.com>
2026-08-25 17:59:47 +0800

    feat: import MotionEvent for map interaction handling

--- android/app/src/main/java/com/hh/geotrack/MainActivity.kt
+++ android/app/src/main/java/com/hh/geotrack/MainActivity.kt
@@ -14,6 +14,7 @@ import android.util.Log
 import android.net.Uri
 import android.content.Intent
 import android.view.View
+import android.view.MotionEvent
 import androidx.core.content.FileProvider
 import java.io.File
 import java.io.FileOutputStream
